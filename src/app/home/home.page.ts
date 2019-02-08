445c71fdceb631be4017415ba2fb54c175a18268
Oddbjørn Lona <pt184@skuld.com>
2019-02-08 11:14:34 +0100

    Added flw for home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LoadingController} from '@ionic/angular';
+import {RestApiService} from '../rest-api.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+    public words: any;
+    public word: 'Lets not forget, remember the PC Deaths.';
+
+    constructor(public api: RestApiService, public loadingController: LoadingController) {
+    }
+
+    ngOnInit() {
+        this.getData();
+    }
+
+
+    async getData() {
+        const loading = await this.loadingController.create({
+            message: 'Loading'
+        });
+
+        await loading.present();
+        this.api.getData()
+            .subscribe(res => {
+                console.log('async getData');
+                this.words = res[0];
+                this.getRandom();
+                loading.dismiss();
+            }, err => {
+                console.log(err);
+                loading.dismiss();
+            });
+    }
+
+    getRandom() {
+        const randomize = Math.floor(Math.random() * this.words.length);
+        this.word = this.words[randomize];
+        console.log(JSON.stringify(this.word));
+        console.log('button click: ' + randomize + ' , ' + this.word);
+    }
+
 
 }
--- src/app/rest-api.service.ts
+++ src/app/rest-api.service.ts
@@ -11,16 +11,31 @@ const apiUrl = 'http://axehigh.com/rpg/flw2/index.php/listdato:1:01-01-2010';
 })
 export class RestApiService {
 
+    wordList: any;
+
     constructor(private http: HttpClient) {
     }
 
     getData(): Observable<any> {
-        const response1 = this.http.get(apiUrl );
-        console.log('Call GetData()');
+        const response1 = this.http.get(apiUrl);
+        console.log('Call restapi.GetData()');
         // let response2= this.http.get(apiUrl+'IN/110001');
         // let response3 = this.http.get(apiUrl+'BR/01000-000');
         // let response4 = this.http.get(apiUrl+'FR/01000');
         return forkJoin([response1/*,response2, response3, response4*/]);
     }
+
+    getDataOnce() {
+        if (this.wordList !== null && typeof this.wordList === 'object') {
+            return this.wordList;
+        } else {
+            const response1 = this.http.get(apiUrl);
+            console.log('Call GetDataOnce()');
+            this.wordList = response1;
+            return this.wordList;
+        }
+
+    }
+
 
 }
