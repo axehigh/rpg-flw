9d2f5013b51c3e0f8df93acdb1772f8a67e12597
olona <oddbjorn_lona@hotmail.com>
2019-02-13 22:52:23 +0800

    Added Catch just in case

--- src/app/rest-api.service.ts
+++ src/app/rest-api.service.ts
@@ -11,27 +11,37 @@ const apiUrl = 'http://axehigh.com/rpg/flw2/index.php/listdato:1:01-01-2010';
 })
 export class RestApiService {
 
-    wordList: Observable <any>;
+    wordList: Observable<any>;
 
     constructor(private http: HttpClient) {
     }
 
     getData(): Observable<any> {
-        const response1 = this.http.get(apiUrl);
-        console.log('Call restapi.GetData()');
-        // let response2= this.http.get(apiUrl+'IN/110001');
-        // let response3 = this.http.get(apiUrl+'BR/01000-000');
-        // let response4 = this.http.get(apiUrl+'FR/01000');
-        return forkJoin([response1/*,response2, response3, response4*/]);
+        try {
+            const response1 = this.http.get(apiUrl);
+            console.log('Call restapi.GetData()');
+            // let response2= this.http.get(apiUrl+'IN/110001');
+            // let response3 = this.http.get(apiUrl+'BR/01000-000');
+            // let response4 = this.http.get(apiUrl+'FR/01000');
+            return forkJoin([response1/*,response2, response3, response4*/]);
+        } catch (e) {
+            console.log(e);
+        }
     }
 
-    getDataOnce(): Observable<any>  {
-        if (!this.wordList) {
-            console.debug('REST API: ' + apiUrl);
+    getDataOnce(): Observable<any> {
+        try {
             const response1 = this.http.get(apiUrl);
             this.wordList = forkJoin([response1]);
+
+            console.log('getDataOnce() ');
+            if (!this.wordList) {
+                console.debug('REST API: ' + apiUrl);
+            }
+            return this.wordList;
+        } catch (e) {
+            console.log(e);
         }
-        return this.wordList;
     }
 
 
